cf5b326c63aa2e5d36d3f495e467d1f4c05cf1ad
Sukum <kungo2009@hotmail.com>
2018-02-28 19:38:14 +0700

    add file init

---
+++ src/client/core/app/init.js
@@ -0,0 +1,32 @@
+(function(app){
+    'use strict';
+
+    angular.module(app.angularModuleName,app.angularDependencies);
+
+    angular
+    .module(app.applicationModuleName)
+    .config(bootstrapConfig);
+
+  bootstrapConfig.$inject = ['$compileProvider', '$locationProvider', '$httpProvider', '$logProvider'];
+
+  function bootstrapConfig($compileProvider, $locationProvider, $httpProvider, $logProvider) {
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    }).hashPrefix('!');
+
+    //$httpProvider.interceptors.push('authInterceptor');
+
+    // Disable debug data for production environment
+    // @link https://docs.angularjs.org/guide/production
+    //$compileProvider.debugInfoEnabled(app.applicationEnvironment !== 'production');
+    //$logProvider.debugEnabled(app.applicationEnvironment !== 'production');
+  }
+
+   angular.element(document).ready(function(){
+    angular.bootstrap(document.querySelector('#core'),[core],{
+           strictDi: true
+       });
+   });
+
+}(ApplicationConfig));
