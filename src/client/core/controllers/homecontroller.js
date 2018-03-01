aa126df599b493973c41117e68181af2f8b40ee5
sukumhomePC <kungo2009@hotmail.com>
2018-03-01 21:26:18 +0700

    add Type and SubType for home page

--- src/client/core/controllers/homecontroller.js
+++ src/client/core/controllers/homecontroller.js
@@ -4,57 +4,58 @@
     angular
         .module('core')
         .controller('HomeController', HomeController)
-    HomeController.$inject = ['$scope', '$state','$filter','IncomeService','MouthY','mySocket']; 
-    function HomeController($scope, $state,$filter,IncomeService,MouthY,mySocket) {
-        var DateSet = function (date,detail){
+    HomeController.$inject = ['$scope', '$state', '$filter', 'IncomeService', 'MouthY', 'mySocket', 'TypeAndSubType'];
+
+    function HomeController($scope, $state, $filter, IncomeService, MouthY, mySocket, TypeAndSubType) {
+        var DateSet = function (date, detail) {
             return $filter('date')(date, detail);
         }
         $scope.data = [];
-        var dataSocket = {};
-        $scope.type = ["รายรับ", "รายจ่าย", "ออมเงิน"];
+        $scope.type = ['รายรับ', 'รายจ่าย', 'เงินออม'];
         $scope.nameuser = 'Sukum';
         $scope.information = {};
         $scope.detail = false;
         $scope.showtype = function () {
-            if ($scope.information.typeof == 0)
-                $scope.subtype = ['เงินเดือน', 'พ่อให้'];
-            else if ($scope.information.typeof == 1)
-                $scope.subtype = ['อาหาร', 'เดินทาง'];
-            else
-                $scope.subtype = ['ออมเงิน'];
-
+            let num = $scope.information.typeof;
+            $scope.subtype = [];
+            TypeAndSubType.getInformation().then(suc => {
+                for(var t = num ; t < suc.length ; t++){
+                    if( suc[t].typeMoney == null) break;
+                    if($scope.type[num] == suc[t].typeMoney ){
+                        $scope.subtype.push(suc[t].subtype)
+                    }
+                }
+            })
         }
-        $scope.skip = function(){
-            if($scope.information.money != (null && "") && $scope.information.typeof != null && ( $scope.information.subtype != null) ){
+        $scope.skip = function () {
+            console.log($scope.information)
+            if ($scope.information.money != (null && "") && $scope.information.typeof != null && ($scope.information.subtype != null)) {
                 $scope.skipp = true;
             }
-        } 
-        $scope.save = function(){
-            
-         
+        }
+        $scope.save = function () {
             $scope.savea = true;
             $scope.information.date = {
-                day: DateSet(Date.now(),"dd"),
-                mouth:MouthY.setMountT(DateSet(Date.now(),"M")),
-                year:MouthY.setYearT(DateSet(Date.now(),'yyyy'))
+                day: DateSet(Date.now(), "dd"),
+                mouth: MouthY.setMountT(DateSet(Date.now(), "M")),
+                year: MouthY.setYearT(DateSet(Date.now(), 'yyyy'))
             }
             $scope.information.typeof = $scope.type[$scope.information.typeof];
-            $scope.information.subtype = $scope.subtype[$scope.information.subtype];  
-            
-            
-            IncomeService.saveInfor($scope.information).then(onSucess).catch(onError);      
+            $scope.information.subtype = $scope.subtype[$scope.information.subtype];
+            IncomeService.saveInfor($scope.information).then(onSucess).catch(onError);
         };
-       
-        function onSucess(res){     
+
+        function onSucess(res) {
             $scope.data.push($scope.information)
             $scope.information = {};
             $scope.skipp = false;
             $scope.subtype = false;
         }
-        function onError(err){
-                console.log(err);
+
+        function onError(err) {
+            console.log(err);
         }
-       
+
     };
 
 }());
