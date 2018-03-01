9bf11e0ed5ff73b0579ab9ccfcbdcff6c7d67e0a
sukumhomePC <kungo2009@hotmail.com>
2018-03-01 22:40:23 +0700

    add script user file layout html and edit main css

--- src/client/core/controllers/homecontroller.js
+++ src/client/core/controllers/homecontroller.js
@@ -4,28 +4,31 @@
     angular
         .module('core')
         .controller('HomeController', HomeController)
-    HomeController.$inject = ['$scope', '$state', '$filter', 'IncomeService', 'MouthY', 'mySocket', 'TypeAndSubType'];
+    HomeController.$inject = ['$scope', '$state', '$filter', 'IncomeService', 'MouthY', 'mySocket', 'TypeAndSubType','Auth'];
 
-    function HomeController($scope, $state, $filter, IncomeService, MouthY, mySocket, TypeAndSubType) {
+    function HomeController($scope, $state, $filter, IncomeService, MouthY, mySocket, TypeAndSubType,Auth) {
         var DateSet = function (date, detail) {
             return $filter('date')(date, detail);
         }
+        $scope.suc = [];
+        TypeAndSubType.getInformation().then(suc => {
+            $scope.suc = suc
+        });
         $scope.data = [];
         $scope.type = ['รายรับ', 'รายจ่าย', 'เงินออม'];
-        $scope.nameuser = 'Sukum';
+        $scope.nameuser = Auth;
+        console.log($scope.nameuser)
         $scope.information = {};
         $scope.detail = false;
-        $scope.showtype = function () {
-            let num = $scope.information.typeof;
+        $scope.showtype = function () {      
             $scope.subtype = [];
-            TypeAndSubType.getInformation().then(suc => {
-                for(var t = num ; t < suc.length ; t++){
-                    if( suc[t].typeMoney == null) break;
-                    if($scope.type[num] == suc[t].typeMoney ){
-                        $scope.subtype.push(suc[t].subtype)
-                    }
+            var t0 = performance.now();
+            for(var t = 0 ; t < $scope.suc.length ; t++){
+                if( $scope.suc[t].typeMoney == null) break;
+                if($scope.type[num] == $scope.suc[t].typeMoney ){
+                    $scope.subtype.push($scope.suc[t].subtype)
                 }
-            })
+            }
         }
         $scope.skip = function () {
             console.log($scope.information)
