a23042fb7ec063c637fb350d30bc2093e53b3e01
Sukum <kungo2009@hotmail.com>
2018-03-07 21:07:20 +0700

    views page add function for number page being doing delete

--- modules/core/client/controllers/viewscontroller.js
+++ modules/core/client/controllers/viewscontroller.js
@@ -6,15 +6,13 @@
         .module('core')
         .controller('ViewsIncomeTotal', ViewsIncomeTotal)
 
-    ViewsIncomeTotal.$inject = ['$scope', 'viewsTest', '$filter', 'MouthY', 'IncomeService', '$state', 'mySocket', '$http', 'Auth', 'TypeAndSubType', '$stateParams'];
+    ViewsIncomeTotal.$inject = ['$scope', 'viewsTest', '$filter', 'MouthY', 'IncomeService', '$state', 'mySocket', '$http', 'Auth', 'TypeAndSubType', '$stateParams','$location'];
     /** @ngInject */
-    function ViewsIncomeTotal($scope, viewsTest, $filter, MouthY, IncomeService, $state, mySocket, $http, Auth, TypeAndSubType, $stateParams) {
+    function ViewsIncomeTotal($scope, viewsTest, $filter, MouthY, IncomeService, $state, mySocket, $http, Auth, TypeAndSubType, $stateParams,$location) {
         var DateSet = function (date, detail) {
             return $filter('date')(date, detail);
         }
         var incomeFunc = (element) => {
-
-
             if (element.typeMoney == "รายรับ") {
                 $scope.inmoney += parseInt(element.moneyInput);
 
@@ -26,33 +24,41 @@
                 $scope.saveMoney += parseInt(element.moneyInput)
 
             }
-            /**/
+            /*ตรวจสอบรายรับรายจ่าย เพื่อทำการคำนวน หา รายรับรายจ่าย*/
+        }
+        $scope.currentPage = 1;
+        $scope.numPerPage = 7;
+      
+        var numberPage = () => {
+            $scope.$watch("currentPage + numPerPage", function() {
+                var begin = ($scope.currentPage - 1) * $scope.numPerPage
+                , end = begin + $scope.numPerPage;
+            
+                $scope.filteredTodos = $scope.information.slice(begin, end);
+              });
+              
         }
         var check_type = (time) => {
+            $scope.notShow = false;
+            
+            $scope.filteredTodos = [];
             $scope.information = [];
-
-
-    
-
             if ($stateParams.type == 'day') {
                $scope.informationReal.forEach((element ,k ) => {
                     if (DateSet(element.timeCreate, 'dd MM yyyy') == DateSet(time, 'dd MM yyyy')) {
                       incomeFunc(element);
                       $scope.information.push(element);
-                      
+                        
                     }
                 });
-
             } 
             else if ($stateParams.type == 'month') {
                 $scope.informationReal.forEach((element ,k ) => {
                     if (DateSet(element.timeCreate, 'MM yyyy') == DateSet(time, 'MM yyyy')) {
                       incomeFunc(element);
-                      $scope.information.push(element);
-                      
+                      $scope.information.push(element);             
                     }
                 });
-
             }
             else if ($stateParams.type == 'year') {
                 $scope.informationReal.forEach((element ,k ) => {
@@ -62,19 +68,27 @@
                       
                     }
                 });
-
-            }else{
+            }else if($stateParams.type == 'all'){
                 $scope.informationReal.forEach((element ,k ) => {
-                    if (DateSet(element.timeCreate, 'MM yyyy') == DateSet(time, 'MM yyyy')) {
-                      incomeFunc(element);           
+                    if (DateSet(element.timeCreate, 'yyyy') == DateSet(time, 'yyyy')) {
+                      incomeFunc(element);
+                     
                     }
-                });
+                });            
                 $scope.information = $scope.informationReal;
-                
+                $scope.notShow = true;
+            }else{
+                $location.path('/home/views/table/all')
             }
+            numberPage();
 
+            /*ตรวจสอบการค้นหาว่าหาโดยวันที่ เดือน หรือ ปี*/
 
         }
+    
+    
+
+        $scope.showPage = 1;
         $scope.information = [];
         $scope.indexof = 0;
         $scope.authentication = Auth;
@@ -84,12 +98,18 @@
         $scope.delmoney = 0;
         $scope.saveMoney = 0;
         $scope.timeS = new Date();
+        
+        
         check_type(Date.now())
+        
+       
         $scope.timeChnge = () => {
             $scope.inmoney = 0;
             $scope.delmoney = 0;
             $scope.saveMoney = 0;
+            
             check_type($scope.timeS);
+         
         }
         TypeAndSubType.getInformation().then(suc => {
             $scope.suc = suc
@@ -111,20 +131,11 @@
         }
         //totalMoney();
         $scope.dateFormat = function (date) {
-            return DateSet(date, "dd") + ' ' + MouthY.setMountT(DateSet(date, "MM")) + ' ' + MouthY.setYearT(DateSet(date, "yyyy"))
+            return  DateSet(date,'hh:mma');
         }
-
-        $scope.year = function (date) {
-            return MouthY.setYearT(DateSet(date, "yyyy"));
+        $scope.dateSetNew = (date)=>{
+            return 'วันที่ทำรายการ : ' + DateSet(date,'dd')+ ' ' + MouthY.setMountT(DateSet(date,'MM')) + ' ' + MouthY.setYearT(DateSet(date,'yyyy'))
         }
-        $scope.mouth = function (date) {
-            return MouthY.setMountT(DateSet(date, "MM"));
-
-        }
-        $scope.day = function (date) {
-            return DateSet(date, "dd");
-        }
-
         $scope.deleteInFor = function (infor) {
             IncomeService.delete(infor).then(function (res) {
 
@@ -156,8 +167,20 @@
             });
         }
 
+ /** สำหรับคิดเลขเภท */
+   
+        numberPage();
 
-
+        $scope.t = [];
+        const v = Math.ceil($scope.information.length / $scope.numPerPage);
+        for(var i = 0 ; i < v;i++){
+            $scope.t[i]=i+1;
+        }
+        $scope.changePage = (n)=>{
+            $scope.currentPage = n;
+            numberPage();
+            $scope.showPage = n;
+        }
     }
 
 }());
