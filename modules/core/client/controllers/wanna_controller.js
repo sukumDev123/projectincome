2a29f59ef9c31acb293c29242a7927efb4b043ff
Sukum <kungo2009@hotmail.com>
2018-03-22 19:44:55 +0700

    add img

---
+++ modules/core/client/controllers/wanna_controller.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict'
+    angular.module('core').controller('WannaController', WannaController);
+    WannaController.$inject = ['$scope'];
+
+    function WannaController($scope) {
+
+    }
+})();
