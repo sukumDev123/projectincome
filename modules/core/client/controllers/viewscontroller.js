5ef90d1a8d531f323fa4c9f46518d9c606b3064e
Sukum <kungo2009@hotmail.com>
2018-04-04 22:23:01 +0700

    edit page view . being will doing pagination .

--- modules/core/client/controllers/viewscontroller.js
+++ modules/core/client/controllers/viewscontroller.js
@@ -12,196 +12,188 @@
         if (!mySocket.connect()) {
             mySocket.connect();
         }
-        var DateSet = (date, detail) => {
+        var filter = (date, detail) => {
             return $filter('date')(date, detail);
         }
-
-
-
-        $scope.type_push_show = false
-        $scope.incomeTotal = viewsTest // status first infor total
-        $scope.currentPage = 1;
-        $scope.numPerPage = 7;
-        $scope.inmoney = 0;
-        $scope.delmoney = 0;
-        $scope.saveMoney = 0;
-        $scope.filteredTodos = []; // for show
-        var show_type_boolen = false;
-        $scope.showType = () => {
-            show_type_boolen = true;
-            $scope.view_date($scope.chDate);
-
-        }
-
-        var showType_r = (type, ele) => {
-            if (ele.typeMoney == type) {
-                $scope.information.push(ele);
-                incomeFunc(ele)
-                page_show()
-
-            }
-
-        }
-
-        var check_num_page = (num) => {
-
-            $scope.t = [];
-            var v = Math.ceil(num / $scope.numPerPage);
-            for (var i = 0; i < v; i++) {
-                $scope.t[i] = i + 1;
-            }
-        }
-        $scope.changePage = n => {
-            if(n > 0 && n <= $scope.t.length ){
-                $scope.currentPage = n;
-                $scope.showPage = n;
-                page_show()
-            }
-            
-
-
-
-        }
-        $scope.loads = () => {
-            $window.a = true;
-            alert($window.a);
-        }
-        var incomeFunc = (element) => {
-
-            if (element.typeMoney == "รายรับ") {
-                $scope.inmoney += parseInt(element.moneyInput);
-            } else if (element.typeMoney == "รายจ่าย") {
-                $scope.delmoney += parseInt(element.moneyInput);
-            } else if (element.typeMoney == 'เงินออม') {
-                $scope.saveMoney += parseInt(element.moneyInput);
+        var data_input = viewsTest; // include data for db.;
+        let temp_in = [],
+            temp_out = [],
+            temp_save = [],
+            err_i = [];
+        var check_type = ele => {
+            /** Check type of list from a come in input. .
+             * For Keep value 
+             * 
+             */
+            if (ele.typeMoney === 'รายรับ') {
+                temp_in.push(ele)
+            } else if (ele.typeMoney === 'รายจ่าย') {
+                temp_out.push(ele);
+            } else if (ele.typeMoney === 'เงินออม') {
+                temp_save.push(ele);
             } else {
-                $scope.inmoney = 0;
-                $scope.delmoney = 0;
-                $scope.saveMoney = 0;
+                err_i.push(ele)
             }
-            /*ตรวจสอบรรายจ่าย	ายรับรายจ่าย เพื่อทำการคำนวน หา รายรับรายจ่าย*/
         }
-        var page_show = () => {
-            var begin = ($scope.currentPage - 1) * $scope.numPerPage,
-                end = begin + $scope.numPerPage;
-            $scope.filteredTodos = $scope.information.slice(begin, end);
-            check_num_page($scope.information.length)
-
+        var showData_date_all = () => {
+            temp_in = [];
+            temp_out = [];
+            temp_save = [];
+            err_i = [];
+            let temp = {};
+            data_input.forEach((ele, index) => {
+                check_type(ele)
+            })
+            return temp = {
+                temp_in,
+                temp_out,
+                temp_save,
+                err_i
+            };
 
         }
+        var showData_data_fix = (date_type, type) => {
+            /** Show Data form date type ex. day , month or year */
+            /**
+             * date_type = Date input;
+             * type = Change ex. dd MM yyyy == day 
+             * MM yyyy == month
+             * yyyy == year
+             */
 
-
-        $scope.deleteInFor = (infor, i) => {
-
-            $scope.information.splice(i, 1)
-            $scope.incomeTotal.splice(i, 1)
-            IncomeService.delete(infor).then(
-                suc => {
-                    Notification.success({
-                        message: suc.mess,
-                        delay: 2000
-                    })
-                    $scope.view_date($scope.chDate)
-                    page_show();
-                }
-            ).catch(
-                err => {
-
-                    $scope.error = err;
-                })
-        }
-        $scope.notShow = false;
-        $scope.timeS = new Date();
-        $scope.view_date = (date) => {
-            $scope.t = [];
-            $scope.inmoney = 0;
-            $scope.delmoney = 0;
-            $scope.saveMoney = 0;
-            $scope.information = [];
-            $scope.chDate = date;
-            $scope.incomeTotal.forEach((element, k) => {
-
-                if (date == 'all') {
-                    $scope.chDate = 'all';
-                    if (show_type_boolen) {
-                        showType_r($scope.type_push_show, element);
-                    } else {
-                        $scope.information.push(element);
-                        incomeFunc(element)
-                        page_show();
-                        $scope.type_push_show = null;
-                    }
-
-
-                } else if (date == 'day') {
-                    if (DateSet(element.timeCreate, 'dd MM yyyy') == DateSet($scope.timeS || Date.now(), 'dd MM yyyy')) {
-                        $scope.chDate = 'day';
-
-                        if (show_type_boolen) {
-                            showType_r($scope.type_push_show, element);
-                        } else {
-                            $scope.information.push(element);
-                            incomeFunc(element)
-                            $scope.type_push_show = null;
-
-
-                        }
-                        page_show();
-
-                    }
-                } else if (date == 'month') {
-                    if (DateSet(element.timeCreate, 'MM yyyy') == DateSet($scope.timeS || Date.now(), 'MM yyyy')) {
-
-                        $scope.chDate = 'month';
-                        if (show_type_boolen) {
-                            showType_r($scope.type_push_show, element);
-                        } else {
-                            $scope.information.push(element);
-                            incomeFunc(element)
-                            page_show();
-                            $scope.type_push_show = null;
-
-
-                        }
-
-                    }
-                } else if (date == 'year') {
-                    if (DateSet(element.timeCreate, 'yyyy') == DateSet($scope.timeS || Date.now(), 'yyyy')) {
-                        $scope.chDate = 'year';
-                        if (show_type_boolen) {
-                            showType_r($scope.type_push_show, element);
-                        } else {
-                            $scope.information.push(element);
-                            incomeFunc(element)
-                            page_show();
-                            $scope.type_push_show = null;
-
-                        }
-
-                    }
+            temp_in = [];
+            temp_out = [];
+            temp_save = [];
+            err_i = [];
+            let temp = {};
+            data_input.forEach((ele, i) => {
+                if (filter(ele.timeCreate, type) === filter(date_type, type)) {
+                    check_type(ele)
                 }
             })
+            return temp = {
+                temp_in,
+                temp_out,
+                temp_save,
+                err_i
+            };
+        }
+        var array_for_show_Html = () => { //** Show All Data. */
+            $scope.array_show_on_browser = [];
+            $scope.show_data_for_html.temp_in.forEach(ele => $scope.array_show_on_browser.push(ele));
+            $scope.show_data_for_html.temp_out.forEach(ele => $scope.array_show_on_browser.push(ele));
+            $scope.show_data_for_html.temp_save.forEach(ele => $scope.array_show_on_browser.push(ele))
+        }
 
-            if ($scope.information.length == 0) {
-                $scope.filteredTodos = [];
+        $scope.show_date = (type) => {
+            /**
+             * type == day or month or year
+             * for keep value in array for show on browser
+             */
+            $scope.array_show_on_browser = [];
+            $scope.show_data_for_html = {};
+            if (type == 'all') {
+                $scope.show_data_for_html = showData_date_all();
+            } else if (type == 'day') {
+                $scope.show_data_for_html = showData_data_fix((($scope.date_input) ? $scope.date_input : Date.now()), 'dd MM yyyy');
+            } else if (type == 'month') {
+                $scope.show_data_for_html = showData_data_fix((($scope.date_input) ? $scope.date_input : Date.now()), 'MM yyyy');
+            } else if (type == 'year') {
+                $scope.show_data_for_html = showData_data_fix((($scope.date_input) ? $scope.date_input : Date.now()), 'yyyy');
             }
-            show_type_boolen = false;
-            if ($scope.type_push_show == null) {
-                $scope.sub_type_show = false
+            $scope.showType = true;
+            array_for_show_Html();
+            $scope.pageNum();
+            if ($scope.array_show_on_browser.length == 0) {
+                $scope.showType = false
+                console.log('null')
             }
         }
-        $scope.timeChnge = () => {
 
-            $scope.view_date($scope.chDate)
+        $scope.type_showF = (type) => {
+            $scope.array_show_on_browser = [];
+            if (type === 'รายรับ') {
+                $scope.show_data_for_html.temp_in.forEach(ele => $scope.array_show_on_browser.push(ele));
+
+            } else if (type === 'รายจ่าย') {
+                $scope.show_data_for_html.temp_out.forEach(ele => $scope.array_show_on_browser.push(ele));
+
+            } else if (type === 'เงินออม') {
+                $scope.show_data_for_html.temp_save.forEach(ele => $scope.array_show_on_browser.push(ele))
+            }
             page_show()
         }
-        $scope.view_date('all')
-        page_show();
+        $scope.deleteInFor = index => {
+            /*$scope.array_show_on_browser = $scope.array_show_on_browser.splice($scope.array_show_on_browser.indexOf(index),1);
+            console.log(index)*/
+        }
+
+
+        /*************************************************** End show Data 1 */
+        $scope.pageNum = () => {
+            $scope.currentPage = 1;
+            $scope.pageSize = 10;
+            page_show();
+        }
+        $scope.changePage = (n) => {
+            let num_input = page_num();
+
+            let num = $scope.t.length;
+            if (n > 0 && n <= num) {
+                $scope.currentPage = n;
+                $scope.pageSize = 10;
+                page_show();
+                if (num_input > num) {
+                    number_show(n + 1);
+
+                }else{
+                    number_show($scope.currentPage);
+                }
+            }
+
+        }
+        var page_num = () => {
+            return Math.ceil($scope.array_show_on_browser.length / $scope.pageSize)
+        }
+        var number_show = (n) => {
+            let num_input = page_num();
+            $scope.t = []
+            let num = 0;
+            num = (n ? n : 3);
+            for (var i = 0; i < num; i++) {
+                $scope.t.push(i + 1);
+            }
+
+
+
+        }
+        var page_show = () => {
+            number_show(0);
+            let begin = ($scope.currentPage - 1) * $scope.pageSize;
+            let end = begin + $scope.pageSize;
+            $scope.array_show_on_browser_html = $scope.array_show_on_browser.slice(begin, end);
+        }
+
+
+
+        /**Start  */
+        $scope.show_date('all');
+
+        $scope.page_end = page_num();
+
+
+
+
+
+
+
+
+
 
         mySocket.on('showNew', data => {
-            $scope.incomeTotal[$scope.incomeTotal.length] = data // status first infor total
+            /*$scope.incomeTotal[$scope.incomeTotal.length] = data // status first infor total
             $scope.view_date($scope.chDate)
-            page_show()
+            page_show()*/
         })
     }
 
